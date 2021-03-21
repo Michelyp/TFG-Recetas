402562ed2288fdc8b11671bd8773b78521c084b2
erianvc <erianvc.dev@gmail.com>
2021-03-21 00:33:21 -0500

    refactor: aplica formateo al proyecto

--- pages/index.vue
+++ pages/index.vue
@@ -27,46 +27,41 @@
 </template>
 
 <script>
-export default {
-  methods: {
-    async writeToRealtimeDb() {
-      const messageRef = this.$fire.database.ref('message');
-      try {
-        await messageRef.set({
-          message: 'Nuxt-Fire with Firebase Realtime Database rocks!',
-        });
-      } catch (e) {
-        alert(e);
-        return;
-      }
-      alert('Success.');
+  export default {
+    methods: {
+      async writeToRealtimeDb() {
+        const messageRef = this.$fire.database.ref('message');
+        try {
+          await messageRef.set({
+            message: 'Nuxt-Fire with Firebase Realtime Database rocks!',
+          });
+        } catch (e) {
+          alert(e);
+          return;
+        }
+        alert('Success.');
+      },
     },
-  },
-};
+  };
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  @apply my-0 mx-auto min-h-screen flex justify-center items-center text-center;
-}
+<style lang="postcss">
+  .container {
+    @apply my-0 mx-auto min-h-screen flex justify-center items-center text-center;
+  }
 
-.title {
-  @apply text-black font-roboto block font-normal;
-  font-size: 100px;
-  letter-spacing: 1px;
-}
+  .title {
+    @apply text-black font-roboto block font-normal;
+    font-size: 100px;
+    letter-spacing: 1px;
+  }
 
-.subtitle {
-  @apply font-normal text-4xl text-black pb-4;
-  word-spacing: 5px;
-}
+  .subtitle {
+    @apply font-normal text-4xl text-black pb-4;
+    word-spacing: 5px;
+  }
 
-.links {
-  @apply pt-4;
-}
+  .links {
+    @apply pt-4;
+  }
 </style>
